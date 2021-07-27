d9713658d1264a6cfeeb1c9f872fa8c3f9056de9
Nizar Allibhoy <nizarallibhoy@gmail.com>
2021-07-27 08:37:38 -0700

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.4.0"),
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0.0"),
     ],
     targets: [
         .target(
